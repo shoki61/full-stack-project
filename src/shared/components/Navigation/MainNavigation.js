4c7590546821cb1fdf992f1607dbed7d61838966
Sohrat <sohrat6128@gmail.com>
2021-02-03 14:35:44 +0300

    set open and close drawer method

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -10,18 +10,18 @@ import './MainNavigation.css';
 const MainNavigation = props => {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    const openDrawer = () => setDrawerIsOpen(true);
-    const closeDrawer = () => setDrawerIsOpen(false);
+    const openDrawerHandler = () => setDrawerIsOpen(true);
+    const closeDrawerHandler = () => setDrawerIsOpen(false);
     return (
         <Fragment>
-            { drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
-            { drawerIsOpen && <SideDrawer>
+            { drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
                 <nav className='main-navigation__drawer-nav'>
                     <NavLinks/>
                 </nav>
-            </SideDrawer>}
+            </SideDrawer>
             <MainHeader>
-                <button onClick={openDrawer} className='main-navigation__menu-btn'>
+                <button onClick={openDrawerHandler} className='main-navigation__menu-btn'>
                     <span/>
                     <span/>
                     <span/>
