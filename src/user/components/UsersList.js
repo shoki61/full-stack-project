eb5c041f4c93967b7dec4fd8e6f73eded8ef3bb6
Sohrat <sohrat6128@gmail.com>
2021-02-03 11:13:38 +0300

    render users

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UserList = props => {
+    if(props.items.length === 0){
+        return (
+            <div className='center'>
+                <h2>No user found.</h2>
+            </div>
+        );
+    };
+    return <ul className='users-list'>
+        { props.items.map(user => (
+            <UserItem 
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                image={user.image}
+                placeCount={user.places}
+            />
+        ))}
+    </ul>
+};
+
+export default UserList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const users = () =>{
-    return <h1>user</h1>
+import UsersList from '../components/UsersList';
+
+const Users = () =>{
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Sohrat Jumadurdyyev',
+            image: 'https://avatars.githubusercontent.com/u/54548249?s=400&u=5ea4ec16b1c673e5c45d92fbca2f74d6b00d7447&v=4',
+            places: 3
+        }
+    ];
+    return <UsersList items={USERS}/>;
 };
 
-export default users;
+export default Users;
