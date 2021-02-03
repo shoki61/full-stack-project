c450b7e45490dd5a0c8939ba44d9a546d7794d95
Sohrat <sohrat6128@gmail.com>
2021-02-03 11:34:51 +0300

    set styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,7 +6,7 @@ const Users = () =>{
     const USERS = [
         {
             id: 'u1',
-            name: 'Sohrat Jumadurdyyev',
+            name: 'Sohrat Jumadurdu',
             image: 'https://avatars.githubusercontent.com/u/54548249?s=400&u=5ea4ec16b1c673e5c45d92fbca2f74d6b00d7447&v=4',
             places: 3
         }
