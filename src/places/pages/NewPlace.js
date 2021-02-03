f4a3dfbfff4352649b7b8f5502e45cbbf0e1e664
Sohrat <sohrat6128@gmail.com>
2021-02-03 10:25:47 +0300

    create new folders and set routers

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewPlace = () => {
+    return <h1>new Place</h1>
+};
+
+export default NewPlace;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const users = () =>{
+    return <h1>user</h1>
+};
+
+export default users;
