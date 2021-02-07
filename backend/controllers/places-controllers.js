53a060677208fc2dec2d54d287d5ece6814ebd51
Sohrat <sohrat6128@gmail.com>
2021-02-07 21:57:53 +0300

    Fetch places using the user id

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -53,15 +53,23 @@ const getPlaceById = async (req, res, next) => {
     res.json({place: place.toObject({getters: true})});
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async(req, res, next) => {
     const userId = req.params.uid;
-    const places = DUMMY_PLACES.filter(p => p.creator === userId);
+
+    let places;
+
+    try {
+      places = await Place.find({ creator:userId });
+    } catch(e) {
+      const error = new HttpError('Not found.', 500);
+      return next(error);
+    };
 
     if (!places || places.length === 0){
         return next(new HttpError('Not found.', 404));
     };
 
-    res.json({places});
+    res.json({places: places.map(place => place.toObject({ getters: true }))});
 };
 
 const createPlace = async (req, res, next) => {
