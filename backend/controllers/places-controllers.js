18306017834bb304a9f041c7002166c67e44288a
Sohrat <sohrat6128@gmail.com>
2021-02-07 18:21:10 +0300

    add validator update and delete routes

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -59,7 +59,7 @@ const createPlace = (req, res, next) => {
 
   if(!errors.isEmpty()){
     throw new HttpError('Invalid inputs passed, please check your data!', 422);
-  }
+  };
 
   const { title, description, address, coordinates, creator } = req.body;
   const createdPlace = {
@@ -80,6 +80,12 @@ const getAllPlaces = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        throw new HttpError('Invalid inputs passed, please check your data!', 422);
+    };
+
     const { title, description } = req.body;
     const placeId = req.params.pid;
 
@@ -94,6 +100,11 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
+
+    if(!DUMMY_PLACES.find(p => p.id === placeId)){
+        throw new HttpError('Not found.', 404);
+    };
+
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
 
     res.status(200).json({message: 'Deleted place.'});
