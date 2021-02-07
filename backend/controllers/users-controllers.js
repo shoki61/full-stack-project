136a8e514296230d9b3860394e406b4304a9bd31
Sohrat <sohrat6128@gmail.com>
2021-02-07 23:26:34 +0300

    fetch users to database

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -19,8 +19,15 @@ const DUMMY_USERS = [
     }
 ];
 
-const getUsers = (req, res, next) => {
-    res.json({users: DUMMY_USERS});
+const getUsers = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    } catch(e) {
+        const error = new HttpError('Fetching users failed, please try again', 500);
+        return next(error);
+    };
+    res.json({users: users.map(user => user.toObject({ getters: true }))});
 };
 
 const singup = async (req, res, next) => {
