c64b299a969efb1e539437f27dc4e7a5b74dc2d5
Sohrat <sohrat6128@gmail.com>
2021-02-07 18:27:48 +0300

    set validator to user routes

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator'); 
 
 const userControllers = require('../controllers/users-controllers');
 
 
 router.get('/', userControllers.getUsers);
 
-router.post('/signup', userControllers.singup);
+router.post('/signup',[
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({min:6})
+], userControllers.singup);
 
 router.post('/login', userControllers.login);
 
