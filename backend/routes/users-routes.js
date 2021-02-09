579332ab524c6507512d61921e092d531d5b5cb7
Sohrat <sohrat6128@gmail.com>
2021-02-09 18:25:37 +0300

    create file upload middleware

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const { check } = require('express-validator'); 
 
 const userControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 
 router.get('/', userControllers.getUsers);
 
-router.post('/signup',[
-    check('name').not().isEmpty(),
-    check('email').normalizeEmail().isEmail(),
-    check('password').isLength({min:6})
-], userControllers.singup);
+router.post(
+    '/signup',
+    fileUpload.single('image'),
+    [
+        check('name').not().isEmpty(),
+        check('email').normalizeEmail().isEmail(),
+        check('password').isLength({min:6})
+    ], 
+    userControllers.singup
+);
 
 router.post('/login', userControllers.login);
 
