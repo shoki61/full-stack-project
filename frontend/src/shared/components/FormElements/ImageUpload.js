b24562378ffd598f8c982881a57babfe714b4cd0
Sohrat <sohrat6128@gmail.com>
2021-02-09 17:57:34 +0300

    set image upload

--- frontend/src/shared/components/FormElements/ImageUpload.js
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -1,13 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import Button from './Button';
 import './ImageUpload.css';
 
 const ImageUpload = props => {
     const filePickerRef = useRef();
+    const [file, setFile] = useState();
+    const [previewUrl, setPreviewUrl] = useState();
+    const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+        if(!file) return;
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPreviewUrl(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    }, [file]);
 
     const pickedHandler = event => {
-
+        let pickedFile;
+        let fileIsValid = isValid;
+        if(event.target.files && event.target.files.length !== 0){
+            pickedFile = event.target.files[0];
+            setFile(pickedFile);
+            setIsValid(true);
+            fileIsValid = true;
+        }else {
+            setIsValid(false);
+            fileIsValid = false;
+        };
+        props.onInput(props.id, pickedFile, fileIsValid);
     };
 
     const pickImageHandler = () => {
@@ -23,10 +46,12 @@ const ImageUpload = props => {
         />
         <div className={`image-upload ${props.center && 'center'}`}>
             <div className='image-upload__preview'>
-                <img src='' alt='Preview'/>
+                { previewUrl && <img src={previewUrl} alt='Preview'/> }
+                { !previewUrl && <p>Please pick a image</p> }
             </div>
             <Button type='button' onClick={pickImageHandler}>PICK IMAGE</Button>
         </div>
+        { !isValid && <p>{props.errorText}</p> }
     </div>
 };
 
--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -36,7 +36,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -46,6 +47,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: '',
+            isValid: false
+          },
+          image: {
+            value: null,
             isValid: false
           }
         },
@@ -111,7 +116,7 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
-          { !isLoginMode && <ImageUpload center id='image'/> }
+          { !isLoginMode && <ImageUpload center id='image' onInput={inputHandler}/> }
           <Input
             element="input"
             id="email"
