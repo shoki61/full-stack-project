a3d8fe594d8403b554f2480a1c47d28e84800c90
Sohrat <sohrat6128@gmail.com>
2021-02-10 10:56:18 +0300

    Set user token using the jsonwebtoken package

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -54,7 +55,18 @@ const singup = async (req, res, next) => {
         const error = new HttpError('Signing up failed, please try again later.', 500);
         return next(error);
     };
-    res.status(201).json({user: createdUser.toObject({ getters: true })});
+
+    let token;
+
+    try {
+        token = jwt.sign({userId: createdUser.id, email: createdUser.email}, 'supersecret_dont_share', { expiresIn: '1h' });
+    } catch(error) {
+        const error = new HttpError('Signing up failed, please try again later.', 500);
+        return next(error);
+    };
+
+
+    res.status(201).json({userId: createdUser.id, email: createdUser.email, token});
 };
 
 const login = async (req, res, next) => {
@@ -86,7 +98,24 @@ const login = async (req, res, next) => {
         return next(error);
     };
 
-    res.json({message: 'Logged in', user: existingUser.toObject({ getters: true })});
+    let token;
+
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email }, 
+            'supersecret_dont_share', 
+            { expiresIn: '1h' }
+        );
+    } catch(error) {
+        const error = new HttpError('Loging in failed, please try again later.', 500);
+        return next(error);
+    };
+
+    res.json({
+        userId: existingUser.id,
+        email: existingUser.email,
+        token
+    });
 };
 
 
